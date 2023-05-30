b585e6fec634cd5111aa6af68d06666f1ec0f266
temasaguru <temasaguru@nalogart.com>
2023-05-30 22:44:22 +0900

    chore: コメント追加

--- prebuild.js
+++ prebuild.js
@@ -1,16 +1,18 @@
-/**
- * NextがVercelで想定している環境変数にNetlifyが対応しておらず
- * 下書きプレビューが失敗するため、ビルド前に修正する
- */
-
-console.log(
-  '[prebuild.js] Fixing build issue. Details: https://github.com/vercel/next.js/issues/49169'
-);
 const path = require('node:path');
 const fs = require('fs');
 const baseDir = process.cwd();
 
+/**
+ * NextがVercelで想定している環境変数にNetlifyが対応しておらず
+ * 下書きプレビューが失敗するため、ビルド前に修正する
+ * TODO: [ここ](https://github.com/vercel/next.js/issues/49169)で修正が報告されたら削除
+ * @see https://answers.netlify.com/t/server-edge-not-defined-error-on-nextjs-ssr-functions-cause-site-to-return-500-errors/91793/31
+ */
 const prebuildScripts = async () => {
+  console.log(
+    '[prebuild.js] Fixing build issue. Details: https://github.com/vercel/next.js/issues/49169'
+  );
+
   const file = path.join(
     baseDir,
     '/node_modules',
